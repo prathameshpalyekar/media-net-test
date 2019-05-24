3e8f9c5513114c06bb79e69c12ba1aa38114a03a
prathameshpalyekar <pratham2604@gmail.com>
2019-05-24 23:47:07 +0530

    Add charts to stock component

--- app/modules/home/components/index.jsx
+++ app/modules/home/components/index.jsx
@@ -1,49 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { subscribeData } from '../actions/subscribeData';
+import React from 'react';
 import Stock from './stock/stock';
-const shortid = require('shortid');
 
 import './home.less';
 
-class Home extends Component {
-    getArragedStocks() {
-        const { stocks } = this.props;
-        const arrangedStocks = [];
-        for (let index = 0; index < stocks.length; index += 3) {
-            const stocksRow = [];
-            const stock0 = stocks[index];
-            const stock1 = stocks[index + 1];
-            const stock2 = stocks[index + 2];
-            stock0 && stocksRow.push(stock0);
-            stock1 && stocksRow.push(stock1);
-            stock2 && stocksRow.push(stock2);
-            const data = {
-                id: shortid.generate(),
-                stocksRow,
-            };
-            arrangedStocks.push(data);
-        }
-        return arrangedStocks;
-    }
-
-    render() {
-        return (
-            <div className="home-dashboard">
-                <div className="-stock-table">
-                    {this.getArragedStocks().map((stocksRowData) => {
-                        const { id, stocksRow } = stocksRowData;
-                        return (
-                            <div key={id} className="row">
-                                {stocksRow.map((stock) => <Stock stock={stock} key={stock.name}/> )}
-                            </div>
-                        )
-                    })}
-                </div>
+const Home = (props) => {
+    const { stocks } = props;
+    return (
+        <div className="home-dashboard">
+            <div className="-stock-table">
+                {stocks.map((stock) => {
+                    const { name } = stock;
+                    return (
+                        <Stock stock={stock} key={stock.name}/>
+                    )
+                })}
             </div>
-        );
-    };
+        </div>
+    );
 }
 
 export default Home;
--- app/modules/home/components/stock/stock.jsx
+++ app/modules/home/components/stock/stock.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import TimeAgo from 'react-timeago';
 import cx from 'classnames';
+import moment from 'moment';
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
 
 const getPrettyNumber = (value) => parseFloat(value, 10).toFixed(2);
 
@@ -10,15 +20,34 @@ const stock = (props) => {
         'up' : currentValue > lastValue,
         'down': currentValue < lastValue
     });
+    const chartData = data.slice(-5).map((datum) => {
+        const { value, time } = datum;
+        const timeValue = moment(time).format('mm:ss');
+        return {
+            value,
+            time: timeValue,
+        };
+    })
 
     return (
-        <div className="col-sm-4 -stock">
+        <div className="-stock">
             <div className="-stock-row">
                 <div className="-name">{name}</div>
                 <div className={currentValueClass}>{getPrettyNumber(currentValue)}</div>
                 <div className="-time">
                     <TimeAgo date={updatedAt} />
                 </div>
+            </div>
+            <div className="-stock-chart">
+                <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart width={400} height={150} data={chartData}>
+                        <CartesianGrid strokeDasharray="4 4" />
+                        <XAxis dataKey="time"/>
+                        <YAxis />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
+                    </AreaChart>
+                </ResponsiveContainer>
             </div>
         </div>
     );
